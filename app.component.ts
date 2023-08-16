e52bc8500d9712e78dbf92d3c32e8f81b201dd59
ramed12 <ramedmendes@gmail.com>
2023-08-15 20:05:51 -0400

    angular sis

---
+++ app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  userName = 'Joaquim';
+
+  userData = {
+    email: 'joaquim@email.com',
+    role: 'Admin',
+  }
+
+  title = 'curso-angular';
+}
